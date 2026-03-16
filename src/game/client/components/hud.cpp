TeeworldsArchive/teeworlds

commit 44beffd36790964f75818fff2c9bdecf399ae54f

fixed compiling

src/game/client/components/hud.cpp
@@ -943,7 +943,10 @@ void CHud::RenderLocalTime(float x)
 	//draw the text
 	char aTimeStr[6];
 	str_timestamp_format(aTimeStr, sizeof(aTimeStr), "%H:%M");
-	TextRender()->Text(0, x-25.0f, (12.5f - 7.5f) / 2.f, 5.0f, aTimeStr, -1);
+	static CTextCursor s_Cursor(5.0f);
+	s_Cursor.Reset(0);
+	s_Cursor.MoveTo(x - 25.0f, (12.5f - 7.5f) / 2.f);
+	TextRender()->TextOutlined(&s_Cursor, aTimeStr, -1);
 }
 
 void CHud::OnMessage(int MsgType, void *pRawMsg)

commit f9c6dcf7291e89d7115afca5a72ee3c26b061956

Fix the score hud getting stuck on the same rank
caused by 44beffd36790964f75818fff2c9bdecf399ae54f

src/game/client/components/hud.cpp
@@ -370,7 +370,7 @@ void CHud::RenderScoreHud()
 
 				TextRender()->TextDeferred(&s_ScoreCursors[t], aBuf, -1);
 
-				s_PositionCursors[t].Reset(0);
+				s_PositionCursors[t].Reset();
 				str_format(aBuf, sizeof(aBuf), "%d.", aPos[t]);
 				s_PositionCursors[t].m_FontSize = 10.0f;
 				TextRender()->TextDeferred(&s_PositionCursors[t], aBuf, -1);